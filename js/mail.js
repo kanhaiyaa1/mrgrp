f95a1bd0a2e4e12892b95284d1652b03b03c668f
digvijay <digvijaykesare1123@gmail.com>
2025-08-26 20:37:12 +0530

    digvijay commit

---
+++ js/mail.js
@@ -0,0 +1,17 @@
+function SendMail() {
+    var parms = {
+        full_name: document.getElementById("full_name").value,
+        email_id: document.getElementById("email_id").value,
+        phone: document.getElementById("phone").value,
+        message: document.getElementById("message").value,
+    };
+
+    emailjs.send("service_ohplkxl", "template_ditisl9", parms)
+        .then(function (response) {
+            alert("Message Sent Successfully!");
+            document.getElementById("contact-form").reset();
+        }, function (error) {
+            alert("Failed to send message. Please try again!");
+            console.error(error);
+        });
+}
